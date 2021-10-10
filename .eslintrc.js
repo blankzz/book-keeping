f3b27860b1324136d63e1d7431a8c1e1488b588d
blankzz <454257312@qq.com>
2021-10-11 00:39:20 +0800

    chore(eslint): allowExpressions for no return

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/explicit-module-boundary-types': {
+      'allowExpressions': true
+    }
   },
   overrides: [
     {
