a2148024af2ef1f039cfe9cb8dc892aa8aa81d1f
blankzz <454257312@qq.com>
2021-12-11 14:36:12 +0800

    feat: 添加 layout 组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-wrapper">
+    <div class="content">
+      <slot />
+    </div>
+    <Nav />
+  </div>
+</template>
+<script lang="ts" setup>
+import Nav from './Nav.vue';
+</script>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
+</style>
